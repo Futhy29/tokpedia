90851e1ab32bb730a97dd4aa6805f4ffa12dfaa4
FIRDAUS <119123432+firdaus-rx@users.noreply.github.com>
2023-06-18 12:25:56 +0700

    MENAMBAH SCROLLING NAVBAR SCRIPT JS

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('navbar-js');
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('navbar-js');
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
